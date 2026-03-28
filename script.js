77214cb4b62129784171ff616f7f7550b5a44252
Rajan Daniel <rajandaniel118@gmail.com>
2026-03-27 23:53:20 -0500

    feat: funtionality of random quote generator

---
+++ script.js
@@ -0,0 +1,29 @@
+const quote = document.getElementById(`quote`)
+const author = document.getElementById(`author`)
+const quoteBtn = document.getElementById(`new-quote-btn`)
+const twitterBtn = document.getElementById(`twitter-btn`)
+
+let currQuote = ""
+let currAuthor = ""
+
+async function getQuote() {
+  const response = await fetch('https://dummyjson.com/quotes/random')
+    .then(res => res.json())
+    .then(data => {
+      currQuote = data.quote;
+      currAuthor = data.author;
+    });
+}
+
+quoteBtn.addEventListener("click", () => {
+  getQuote();
+  quote.textContent = currQuote;
+  author.textContent = currAuthor;
+});
+
+/* ON PAGE LOAD RUN */
+document.addEventListener("DOMContentLoaded", () => {
+  getQuote();
+  quote.textContent = currQuote;
+  author.textContent = currAuthor;
+});
